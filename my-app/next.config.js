670f64669ff40bfda802f7c6f571bfd232ecc653
ayazibrahimov <ayaz.ibrahimov.1998@mail.ru>
2024-01-20 12:40:01 +0400

    configiration

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
       // output:"export",
       reactStrictMode:false,
       images: {unoptimized:true} ,
@@ -8,12 +7,6 @@ const nextConfig = {
         maxInactiveAge: 1000 * 60 * 60,
         pagesBufferLength: 5,
       }
-=======
-    // distDir: 'build',
-     // output:'export',
-     // reactStrictMode:false,
-     // images: {unoptimized:true} ,
->>>>>>> 2819e1a52b99fdfe2235f4d339b695c7f8147692
 }
 
 
