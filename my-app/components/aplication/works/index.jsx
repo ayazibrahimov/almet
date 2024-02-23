bf052a27d14e9853b8c32c41beb9a4481880ce15
Musa <59684228+Musa-Hesenli@users.noreply.github.com>
2024-02-23 14:44:31 +0400

    fix

--- my-app/components/aplication/works/index.jsx
+++ my-app/components/aplication/works/index.jsx
@@ -23,7 +23,7 @@ const elementStyle = positionData === 'firstElement'
 
         <ul className={styles.list}>
             {
-              application.map((data,index)=>(
+              application?.map((data,index)=>(
                 <li key={data.id}> <span style={{animationDuration:`${0+data.id}s` }}></span> {data.title}</li>
               ))
             }
