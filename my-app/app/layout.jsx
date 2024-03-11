575e1924a2073e94255a9091f753bb012c2c9bc6
Ayaz <88205516+ayazibrahimov@users.noreply.github.com>
2024-03-11 16:02:07 +0400

    Update layout.jsx

--- my-app/app/layout.jsx
+++ my-app/app/layout.jsx
@@ -10,8 +10,8 @@ const opensans = Open_Sans({ subsets: ['latin'] })
 
 export const metadata = {
   title:{
-    default:'Home',
-    template:'%s | My title'
+    default:'Wire Mesh',
+    template:':'Wire Mesh | Home'
   },
   description: 'My site description...'
 }
